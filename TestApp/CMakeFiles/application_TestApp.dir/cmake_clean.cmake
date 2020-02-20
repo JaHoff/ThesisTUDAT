00d467b90be8b68165c2a55ab04b9e226c9f2832
javanthoff <javanthoff@gmail.com>
2020-02-20 12:26:46 +0100

    Init commit

---
+++ TestApp/CMakeFiles/application_TestApp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/application_TestApp.dir/TestApp.cpp.obj"
+  "D:/TUDAT/tudatBundle/tudatApplications/bin/applications/application_TestApp.exe"
+  "D:/TUDAT/tudatBundle/tudatApplications/bin/applications/application_TestApp.exe.manifest"
+  "D:/TUDAT/tudatBundle/tudatApplications/bin/applications/application_TestApp.pdb"
+  "libapplication_TestApp.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/application_TestApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TestApp/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/TUDAT/tudatBundle/tudatApplications/TestApp
+# Build directory: D:/TUDAT/build1/tudatApplications/TestApp
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
